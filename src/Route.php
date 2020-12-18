cc6e37b4f351127dd1172c97ba7462369f4cfa4e
ScurraMagna <55872534+ScurraMagna@users.noreply.github.com>
2020-12-18 10:02:13 +0000

    Update Route.php

--- src/Route.php
+++ src/Route.php
@@ -14,20 +14,17 @@ class Route
 
   protected $slugMarker = "&";
 
-  public function __construct($path, $callable, $slugMarker)
-  {
+  public function __construct($path, $callable, $slugMarker) {
     $this->path = trim($path, '/');  
     $this->callable = $callable;
     $this->slugMarker = $slugMarker;
   }
 
-  public function match($url)
-  {
+  public function match($url) {
     $url = trim($url, '/');
     $path = preg_replace_callback('#'.$this->slugMarker.'([\w]+)#', [$this, 'paramMatch'], $this->path);
     $regex = "#^$path$#i";
-    if(!preg_match($regex, $url, $matches))
-    {
+    if(!preg_match($regex, $url, $matches)) {
       return false;
     }
     array_shift($matches);
@@ -35,48 +32,44 @@ class Route
     return true;
   }
 
-  public function with($param, $regex)
-  {
+  public function with($param, $regex) {
+    $regex = ($regex === "UPPER") ? "/[A-Z]+/" :
+             ($regex === "LOWER") ? "/[a-z]+/" :
+             ($regex === "ALPHA") ? "/[a-zA-Z]+/" :
+             ($regex === "DIGIT") ? "/[0-9]+/" :
+             ($regex === "ALNUM") ? "/[a-zA-Z0-9]+/" :
+             $regex;
     $this->params[$param] = str_replace('(', '(?:', $regex);
     return $this;
   }
 
-  private function paramMatch($match)
-  {
-    if(isset($this->params[$match[1]]))
-    {
+  private function paramMatch($match) {
+    if(isset($this->params[$match[1]])) {
       return '(' . $this->params[$match[1]] . ')';
     }
     return '([^/]+)';
   }
 
-  public function call()
-  {
-    if(is_string($this->callable))
-    {
+  public function call() {
+    if(is_string($this->callable)) {
       $params = explode('#', $this->callable);
       $controller = "App\\Controllers\\" . $params[0] . "Controller";
       $controller = new $controller();
-      if(method_exists($controller, $params[1]))
-      {
+      if(method_exists($controller, $params[1])) {
         return call_user_func_array([$controller, $params[1]], $this->matches);
       }
-      else
-      {
+      else {
         echo "Error 404";
       }
     }
-    else
-    {
+    else {
       return call_user_func_array($this->callable, $this->matches);
     }
   }
 
-  public function getUrl($params)
-  {
+  public function getUrl($params) {
     $path = $this->path;
-    foreach($params as $key => $value)
-    {
+    foreach($params as $key => $value) {
       $path = str_replace(":$key", $value, $path);
     }
     return $path;
